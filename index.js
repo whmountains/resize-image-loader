fc93410e719caf63054d651ba177220a416fc700
puppybits <bobby@levelmoney.com>
2015-09-22 10:43:18 -0700

    [Bug Fixes] fixes for lots of images & gm race conditions
    - added a queue for image processing so there is not too many version of GM running at once. This allows for processing a huge quantity of images.
    - fixed `size.width = null`. Sometimes the GM would run the buffer task before finishing the size inspection. Switched to make one opertation dependant on the other operation.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// Options
+//  @sizes Array of Integers
+//    require('resize-image?sizes[]=200w,sizes[]=900w!./myImage.jpg');
+//
+//  @placeholder Integers (not compatible with sizes)
+//    require('resize-image?placeholder=500!./myImage.jpg');
+//  @blur Integers (not compatible with sizes)
+//    require('resize-image?placeholder&blur=10!./myImage.jpg');
+//
+//  @format String ('jpg', 'gif', 'webp', 'png')
+//    require('resize-image?format=webp!./myImage.jpg');
+
+
 var gm = require('gm').subClass({ imageMagick: true });
 var Datauri = require('datauri');
 var fs = require('fs');
@@ -7,45 +20,90 @@ var defaultSizes = ['320w','960w','2048w'];
 var defaultBlur = 40;
 var defaultPlaceholderSize = 20;
 
-function createPlaceholder(content, placeholder, ext, blur, callback){
-  var size = null;
-  gm(content)
-    .resize(placeholder)
-    .size(function(err, _size){ size = _size; })
-    .toBuffer(ext, function(err, buf){
-      if (!buf) return;
+var queue = (function(q, c){
+  var max = 10;
+  var push = function(fnc){
+      q.push(fnc);
+      canDo();
+    },
+    canDo = function(){
+      if(c < max && q.length > 0){
+        // console.log(q.length + " images remaining.");
+        c++;
+        q.shift()(next);
+      }
+    },
+    next = function(){
+      setTimeout(function(){
+        c--;
+        canDo();
+      },0);
+    };
+    return {push:push, next:next};
+}([], 0));
 
-      var uri = new Datauri().format('.'+ext, buf).content;
-      var blur =  "<svg xmlns='http://www.w3.org/2000/svg' width='100%' viewBox='0 0 " + size.width + " " + size.height + "'>" +
-                    "<defs><filter id='puppybits'><feGaussianBlur in='SourceGraphic' stdDeviation='" + defaultBlur + "'/></filter></defs>" +
-                    "<image width='100%' height='100%' xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='" + uri + "' filter='url(#puppybits)'></image>" +
-                  "</svg>";
-      var micro = new Datauri().format('.svg', new Buffer(blur, 'utf8')).content;
-      callback(null, "module.exports = \""+micro+"\"");
-  });
+function createPlaceholder(content, placeholder, ext, blur, callback, files){
+  return function(next){
+
+    var getSize = function(){
+      gm(content)
+        .size(function(err, _size){
+          if (err) {
+            // console.log('err: ', err);
+            return;
+          }
+          if (!_size) {
+            getSize();
+            return;
+          }
+          // console.log('size: ',_size);
+          setPlaceholder(_size);
+      });
+    };
+
+    var setPlaceholder = function(size){
+      gm(content)
+        .resize(placeholder)
+        .toBuffer(ext, function(err, buf){
+          if (!buf) return;
+          // console.log(files, size);
+          var uri = new Datauri().format('.'+ext, buf).content;
+          var blur =  "<svg xmlns='http://www.w3.org/2000/svg' width='100%' viewBox='0 0 " + size.width + " " + size.height + "'>" +
+                        "<defs><filter id='puppybits'><feGaussianBlur in='SourceGraphic' stdDeviation='" + defaultBlur + "'/></filter></defs>" +
+                        "<image width='100%' height='100%' xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='" + uri + "' filter='url(#puppybits)'></image>" +
+                      "</svg>";
+          var micro = new Datauri().format('.svg', new Buffer(blur, 'utf8')).content;
+          callback(null, "module.exports = \""+micro+"\"");
+          next();
+        });
+    };
+  };
 }
 
 function createResponsiveImages(content, sizes, ext, files, emitFile, callback){
-  var count = 0;
-  var images = [];
-  var imgset = files.map(function(file, i){ return file + ' ' + sizes[i] + ' '; }).join(',');
-  sizes.map(function(size, i){
-    size = parseInt(size);
-    gm(content)
-      .resize(size)
-      .toBuffer(ext, function(err, buf){
-        if (buf){
-          // console.log('output: ', files[i]);
-          images[i] = buf;
-          emitFile(files[i], buf);
-        }
+  return function(next){
+    var count = 0;
+    var images = [];
+    var imgset = files.map(function(file, i){ return file + ' ' + sizes[i] + ' '; }).join(',');
+    sizes.map(function(size, i){
+      size = parseInt(size);
+      gm(content)
+        .resize(size)
+        .toBuffer(ext, function(err, buf){
+          if (buf){
+            // console.log('output: ', files[i]);
+            images[i] = buf;
+            emitFile(files[i], buf);
+          }
 
-        count++;
-        if (count >= files.length) {
-          callback(null, "module.exports = \""+imgset+"\"");
-        }
+          count++;
+          if (count >= files.length) {
+            callback(null, "module.exports = \""+imgset+"\"");
+            next();
+          }
+      });
     });
-  });
+  };
 }
 
 module.exports = function(content) {
@@ -80,11 +138,11 @@ module.exports = function(content) {
       query.placeholder = parseInt(query.placeholder) || defaultPlaceholderSize;
       query.blur = query.blur || defaultBlur;
 
-      createPlaceholder(content, query.placeholder, ext, query.blur, callback);
+      queue.push(createPlaceholder(content, query.placeholder, ext, query.blur, callback, files));
     } else /* return srcset*/ {
 
       var emitFile = this.emitFile;
-      createResponsiveImages(content, sizes, ext, files, emitFile, callback);
+      queue.push(createResponsiveImages(content, sizes, ext, files, emitFile, callback));
     }
   }
 };
